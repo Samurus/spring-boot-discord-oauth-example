06c7f2caad2a0c1fe7604c6bccd64937bb9e9137
Samurus <achenbachj@web.de>
2021-05-03 12:13:53 +0200

    heroku compatibility

--- src/main/java/de/marvn/interception/backend/SuccessEndpoint.java
+++ src/main/java/de/marvn/interception/backend/SuccessEndpoint.java
@@ -1,11 +1,19 @@
 package de.marvn.interception.backend;
 
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/login")
 public class SuccessEndpoint {
 
   @GetMapping("/oauth2/code/discord")
-  public String getCustomers(@RequestHeader Map<String, String> headers) {
+  public ResponseEntity<String> getCustomers(@RequestHeader Map<String, String> headers) {
     return new ResponseEntity<String>(
         String.format("Listed %d headers", headers.size()), HttpStatus.OK);
   }
