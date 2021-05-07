09bdd0725c5940d0540a23f4deb296b77087dd30
Samurus <achenbachj@web.de>
2021-05-08 01:49:28 +0200

    optimization and refactoring

--- src/main/java/de/samurus/discbot/backend/SecurityConfig.java
+++ src/main/java/de/samurus/discbot/backend/SecurityConfig.java
@@ -27,7 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.oauth2Login()
+    http .authorizeRequests(authorizeRequests ->
+        authorizeRequests
+
+            .anyRequest().authenticated()
+    )
+        .oauth2Login()
         .tokenEndpoint().accessTokenResponseClient(accessTokenResponseClient())
         .and()
         .userInfoEndpoint().userService(userService());
